5b64e800c49f68a3f3918ae76da04919265f3af9
LeVanish <mr.ostapenkoiv@gmail.com>
2026-05-26 10:32:06 +1000

    Import order.sol typo fix

--- contracts/delivery.sol
+++ contracts/delivery.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./Order.sol";
+import "./order.sol";
 
 /*
     Delivery Contract:
--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "./Order.sol";
+import "./order.sol";
 
 //TODO: add arbiter logic if time allows
 
